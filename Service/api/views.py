c45f74121fd5f8fad414bd9215ced9f9119793cf
yuvaraj-07 <59510075+yuvaraj-06@users.noreply.github.com>
2022-05-12 19:35:01 -0700

    Created Views.py with initial imports

--- Service/api/views.py
+++ Service/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Employee,Rooms,Reservations,Bookings,Employees_Model,Rooms_Model,Reservations_Model,Bookings_Model
+from .serializers import EmployeeSerializer,RoomsSerializer,ReservationsSerializer,BookingsSerializer
+from rest_framework.decorators import api_view 
+from ninja import NinjaAPI
+from ninja.security import HttpBearer
+
+class GlobalAuth(HttpBearer):
+    def authenticate(self, request, token):
+        if token == "1":
+            return token
+api = NinjaAPI(auth=GlobalAuth(),title="Meeting Room Reservations")
